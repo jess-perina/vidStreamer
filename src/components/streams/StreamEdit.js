caef14083a755de75078cd3a72cc328f8e70c901
Jessica Perina <jperina@gmail.com>
2019-07-11 12:48:46 -0700

    refactor(stream): move shared form logic into parent component for code reuse

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,10 +1,18 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+
+import StreamForm from './StreamForm'
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
+  }
+
+  onSubmit = (formValues) => {
+    console.log(formValues)
+    this.props.editStream(this.props.match.params.id, formValues)
   }
 
   render() {
@@ -14,9 +22,11 @@ class StreamEdit extends Component {
     }
     return (
       <div>
-        {this.props.stream.title}
-        <br />
-        {this.props.stream.description}
+        <h3>Edit Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
       </div>
     )
   }
@@ -28,4 +38,4 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
