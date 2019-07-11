bc082b7516b2ddec60c561e329c1e5941b57984e
Jessica Perina <jperina@gmail.com>
2019-07-10 22:08:43 -0700

    feat: flesh out streamList component and connect to redux

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-const StreamList = () => {
-  return (
-    <div>StreamList</div>
-  )
+import { fetchStreams } from '../../actions'
+
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams()
+  }
+
+  renderStreamList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderStreamList()}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default StreamList
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) }
+}
+
+export default connect(mapStateToProps, {fetchStreams})(StreamList)
 
