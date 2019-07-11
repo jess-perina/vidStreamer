404392520ff95d1f0da432685aac68fe97f16413
Jessica Perina <jperina@gmail.com>
2019-07-11 10:46:17 -0700

    feat: link created streams to users and add admin buttons

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { fetchStreams } from '../../actions'
@@ -8,10 +9,26 @@ class StreamList extends Component {
     this.props.fetchStreams()
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">
+            Edit
+          </button>
+          <button className="ui button negative">
+            Delete
+          </button>
+        </div>
+      )
+    }
+  }
+
   renderStreamList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -22,6 +39,18 @@ class StreamList extends Component {
     })
   }
 
+  renderCreateButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
@@ -29,13 +58,18 @@ class StreamList extends Component {
         <div className="ui celled list">
           {this.renderStreamList()}
         </div>
+        {this.renderCreateButton()}
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) }
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  }
 }
 
 export default connect(mapStateToProps, {fetchStreams})(StreamList)
